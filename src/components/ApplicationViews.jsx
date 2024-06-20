bada0a2ed5fa367dbe86d4df7e5414e90f296133
William Woodard <williamwoodard98@gmail.com>
2024-06-20 14:41:14 -0500

    user can now login

---
+++ src/components/ApplicationViews.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Authorized } from "./Authorized"
+import { Login } from "../pages/Login.jsx"
+import Home from "../pages/Home"
+import { Register } from '../pages/Register.jsx'
+
+
+export const ApplicationViews = () => {
+/*     const [rocksState, setRocksState] = useState([{
+        id: 1,
+        name: "Sample",
+        type: {
+            id: 1,
+            label: "Volcanic"
+        }
+    }])
+
+    const fetchRocksFromAPI = async (showAll) => {
+        let url = "http://localhost:8000/rocks"
+
+        if (showAll !== true) {
+            url = "http://localhost:8000/rocks?owner=current"
+        }
+        const response = await fetch(url,
+            {
+                headers: {
+                    Authorization: `Token ${JSON.parse(localStorage.getItem("rock_token")).token}`
+                }
+            })
+        const rocks = await response.json()
+        setRocksState(rocks)
+    } */
+
+
+    return <BrowserRouter>
+        <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route element={<Authorized />}>
+                <Route path="/" element={<Home />} />
+            </Route>
+        </Routes>
+    </BrowserRouter>
+}
